eb784978932d03155c8cc5b10a6fec6fd5bb34b7
CaioFauza <caiofauza@gmail.com>
2020-11-20 23:33:21 -0300

    Added task routes

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.task, name='get__create_tasks'),
+    path('<int:id>', views.task, name='update__delete_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from tasks.models import Task
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.core import serializers
 
+@csrf_exempt
+def task(request, id=None):
+    if(request.method == 'GET'):
+        tasks = Task.objects.all()
+        return HttpResponse(serializers.serialize('json', tasks), content_type='application/json')
 
-# Create your views here.
+    if(request.method == 'POST'):
+        data = json.loads(request.body)
+        task = Task.objects.create(**data)
+        return HttpResponse(json.dumps({'Status': 'Success', 'Message': 'Task successfully created.', 'Id': task.pk}))
 
+    if(request.method == 'PATCH'):
+        data = json.loads(request.body)
+        update = Task.objects.filter(pk=id).update(**data)
+        if update == 0: return HttpResponseBadRequest(json.dumps({'Status': 'Error', 'Message': 'Invalid task provided.', 'Id': id}))
+        return HttpResponse(json.dumps({'Status': 'Success', 'Message': 'Task successfully updated.', 'Id': id}))
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    if(request.method == 'DELETE'):
+        delete = Task.objects.filter(pk=id).delete()
+        if delete[0] == 0: return HttpResponseBadRequest(json.dumps({'Status': 'Error', 'Message': 'Invalid task provided.', 'Id': id}))
+        return HttpResponse(json.dumps({'Status': 'Success', 'Message': 'Task successfully deleted.', 'Id': id}))
